a36342ea6be8ed51dcec200d666a6eebdc6a7786
Radin <radintsochev@gmail.com>
2025-01-31 02:46:06 +0200

    Made the game gradually increase in difficulty(enemies spawning more and getting faster). Added the score and lives to be shown topleft. Added 3 lives to the player. Added powerups(more movement speed and more attack speed) that can spawn at a given ratein the place of killed enemies, they also despawn if havent been collected for a while. Added the final score to the Game Over screen. Changed the Game Over screen to be able to exit only trough press escape.

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.laser_shoot_time >= self.cooldown_time:
                 self.can_shoot = True
 
-
     def update(self, keys, deltatime):
         self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
         self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
@@ -53,10 +52,11 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, groups, surface):
         super().__init__(groups)
         self.image = surface
-        position = (random.randint(0, SCREEN_WIDTH), 0)
+        enemy_width = enemy_surface.get_size()[0]
+        position = (random.randint(int(enemy_width / 2), SCREEN_WIDTH - int(enemy_width / 2)), 0)
         self.rect = self.image.get_frect(center = position)
         self.direction = pygame.Vector2(0, 1)
-        self.speed = 250
+        self.speed = 250 + 30 * deltatime
 
     def update(self, keys, deltatime):
         self.rect.center += self.direction * self.speed * deltatime
@@ -85,57 +85,110 @@ class Star(pygame.sprite.Sprite):
         self.image = surface
         self.rect = rect
 
+class UpgradeIcon(pygame.sprite.Sprite):
+    def __init__(self, groups, surface, position):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(center = position)
+        self.life = 0
+
+    def update(self, keys, deltatime):
+        if self.life < 5000:
+            self.life += 1000 * deltatime
+        else:
+            self.kill()
+
 def laser_collisions():
+    global enemies_destroyed
+    mov_spd_flag = False
+    att_spd_flag = False
     for laser in laser_sprites:
-        collided_enemies = pygame.sprite.spritecollide(laser, enemy_sprites, True, pygame.sprite.collide_mask)
-        if collided_enemies:
+        collided_enemy = pygame.sprite.spritecollide(laser, enemy_sprites, True, pygame.sprite.collide_mask)
+        if collided_enemy:
             laser.kill()
-            Explosion(all_sprites, explosion_frames, laser.rect.midtop)
+            Explosion(all_sprites, explosion_frames, collided_enemy[0].rect.midtop)
+            if random.uniform(0, 1) < mov_spd_rate:
+                mov_spd_flag = True
+            if random.uniform(0, 1) < att_spd_rate:
+                att_spd_flag = True
+            
+            if mov_spd_flag and not att_spd_flag:
+                UpgradeIcon((all_sprites, mov_spd_sprites), mov_spd_surface, collided_enemy[0].rect.center)
+            elif att_spd_flag and not mov_spd_flag:
+                UpgradeIcon((all_sprites, att_spd_sprites), att_spd_surface, collided_enemy[0].rect.center)
+            elif att_spd_flag and mov_spd_flag:
+                if random.randint(0, 1):
+                    UpgradeIcon((all_sprites, mov_spd_sprites), mov_spd_surface, collided_enemy[0].rect.center)
+                else:
+                    UpgradeIcon((all_sprites, att_spd_sprites), att_spd_surface, collided_enemy[0].rect.center)
+
+            enemies_destroyed += 1
 
 def player_collisions():
-    if pygame.sprite.spritecollide(player, enemy_sprites, True, pygame.sprite.collide_mask):
+    global lives
+    global player
+    collided_enemy = pygame.sprite.spritecollide(player, enemy_sprites, True, pygame.sprite.collide_mask)
+    if collided_enemy:
         damage_sound_effect.play()
-        player.kill()
-        game_over_screen()
+        Explosion(all_sprites, explosion_frames, collided_enemy[0].rect.center)
+        lives -= 1
+        if lives == 0:
+            player.kill()
+            game_over_screen()
 
-def display_time_passsed():
-    current_time = pygame.time.get_ticks() // 1000
-    timer_title_surface = font.render('Time: ', True, (230, 230, 230))
-    timer_title_rect = timer_title_surface.get_frect(topleft = (10, 20))
-    timer_surface = font.render(str(current_time), True, (230, 230, 230))
-    timer_rect = timer_surface.get_frect(topleft = timer_title_rect.topright)
-    screen.blit(timer_title_surface, timer_title_rect)
-    screen.blit(timer_surface, timer_rect)
-    pygame.draw.rect(screen, (230, 230, 230), timer_rect.inflate(15, 15), 5, 5)
+    collided_mov_spd = pygame.sprite.spritecollide(player, mov_spd_sprites, True, pygame.sprite.collide_mask)
+    if collided_mov_spd:
+        upgrade_sound_effect.play()
+        if player.speed < 1000:
+            player.speed += 25
 
+    collided_att_spd = pygame.sprite.spritecollide(player, att_spd_sprites, True, pygame.sprite.collide_mask)
+    if collided_att_spd:
+        upgrade_sound_effect.play()
+        if player.cooldown_time > 0:
+            player.cooldown_time -= 25
+
+def display_game_info(title, info, topleft_point, border = False, color = (230, 230, 230)):
+    title_surface = font.render(f"{title}: ", True, color)
+    title_rect = title_surface.get_frect(topleft = topleft_point)
+    info_surface = font.render(str(info), True, color)
+    info_rect = info_surface.get_frect(topleft = title_rect.topright)
+    screen.blit(title_surface, title_rect)
+    screen.blit(info_surface, info_rect)
+    if border:
+        pygame.draw.rect(screen, (230, 230, 230), info_rect.inflate(15, 15), 5, 5)
+    
 def fade(width, height): 
     fade = pygame.Surface((width, height))
     fade.fill((0,0,0))
     for alpha in range(0, 300):
         fade.set_alpha(alpha)
-        screen.fill('#2c205a')
-        all_sprites.draw(screen)
-        display_time_passsed()
+        draw_window()
         screen.blit(fade, (0,0))
         pygame.display.update()
         pygame.time.delay(5)
 
 def game_over_screen():
     global running
+    global enemies_destroyed
     fade(SCREEN_WIDTH, SCREEN_HEIGHT)
     game_over = True
     game_over_text_surface = game_over_font.render('Game Over', True, (255, 0, 0))
-    game_over_text_rect = game_over_text_surface.get_frect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
-    game_over_info_surface = font.render('Press any key to exit...', True, (255, 0, 0))
+    game_over_text_rect = game_over_text_surface.get_frect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4))
+    game_over_info_surface = font.render('Press ESC to exit...', True, (255, 0, 0))
     game_over_info_rect = game_over_info_surface.get_frect(midtop = game_over_text_rect.inflate(15, 15).midbottom)
     screen.fill('black')
     screen.blit(game_over_text_surface, game_over_text_rect)
     screen.blit(game_over_info_surface, game_over_info_rect)
+    display_game_info('final score', enemies_destroyed, game_over_info_rect.inflate(15, 15).bottomleft, color=(255, 0 , 0))
     pygame.display.flip()
     while game_over:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+            if event.type == pygame.QUIT:
                 game_over = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    game_over = False
     running = False
 
 def generate_stars(num_stars):
@@ -149,19 +202,21 @@ def generate_stars(num_stars):
                 break
             x = random.randint(0, SCREEN_WIDTH - star_width)
             y = random.randint(0, SCREEN_HEIGHT - star_height)
-
-            # Create a rect for the new star
-            new_star_rect = pygame.Rect(x, y, star_width, star_height)
-
-            # Check for overlap with existing stars
-            
+            new_star_rect = pygame.Rect(x, y, star_width, star_height)            
             if not any(new_star_rect.colliderect(existing) for existing in star_positions):
                 star_positions.append(new_star_rect)
-                break  # Exit loop when a valid position is found
+                break 
             else:
                 tries_count += 1
-
     return star_positions
+
+def draw_window():
+    global enemies_destroyed
+    global lives
+    screen.fill('#2c205a')
+    all_sprites.draw(screen)
+    display_game_info('score', enemies_destroyed, (10, 10), True)
+    display_game_info('lives', lives, (10, font.get_height() + 20))
 
 pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
@@ -172,13 +227,17 @@ all_sprites = pygame.sprite.Group()
 enemy_sprites = pygame.sprite.Group()
 laser_sprites = pygame.sprite.Group()
 star_sprites = pygame.sprite.Group()
+att_spd_sprites = pygame.sprite.Group()
+mov_spd_sprites = pygame.sprite.Group()
 
 font = pygame.font.Font(join('game_assets', "Sterion-BLLld.ttf"), 30)
 game_over_font = pygame.font.Font(join('game_assets', "Sterion-BLLld.ttf"), 100)
 
 laser_surface = pygame.image.load(join('game_assets', 'laser.png')).convert_alpha()
-enemy_surface = pygame.image.load(join('game_assets', 'enemy_ship.png')).convert_alpha()
 star_surface = pygame.image.load(join('game_assets', 'star.png')).convert_alpha()
+att_spd_surface = pygame.image.load(join('game_assets', 'attack_speed_icon.png')).convert_alpha()
+mov_spd_surface = pygame.image.load(join('game_assets', 'movement_speed_icon.png')).convert_alpha()
+enemy_surface = pygame.image.load(join('game_assets', 'enemy_ship.png')).convert_alpha()
 explosion_frames = [pygame.image.load(join('game_assets', 'explosion', f'{i}.png')).convert_alpha() for i in range(1, 13)]
 
 laser_sound_effect = pygame.mixer.Sound(join('game_assets', 'sounds', 'laser_sound.mp3'))
@@ -189,6 +248,7 @@ damage_sound_effect = pygame.mixer.Sound(join('game_assets', 'sounds', 'damage_s
 background_music = pygame.mixer.Sound(join('game_assets', 'sounds', 'background_music.mp3'))
 background_music.set_volume(0.1)
 background_music.play(loops= -1)
+upgrade_sound_effect = pygame.mixer.Sound(join('game_assets', 'sounds', 'upgrade.mp3'))
 
 star_rects = generate_stars(70)
 
@@ -198,12 +258,20 @@ player = Player(all_sprites)
 
 running = True
 game_over = False
+enemies_destroyed = 0
+lives = 3
+att_spd_rate = 0.3
+mov_spd_rate = 0.3
+time_passed = pygame.time.get_ticks()
 clock = pygame.time.Clock()
 
+enemy_spawn_cooldown = 2000
 enemy_event = pygame.event.custom_type()
-pygame.time.set_timer(enemy_event, 1000)
+increase_enemies_event = pygame.event.custom_type()
+pygame.time.set_timer(increase_enemies_event, 3500)
 
 while running:
+    current_time = pygame.time.get_ticks()
     deltatime = clock.tick() / 1000
     #event loop
     for event in pygame.event.get():
@@ -211,18 +279,21 @@ while running:
             running = False
         if event.type == enemy_event:
             Enemy((all_sprites, enemy_sprites), enemy_surface)
+        if event.type == increase_enemies_event and enemy_spawn_cooldown > 100:
+            enemy_spawn_cooldown *= 0.92
+            pygame.time.set_timer(enemy_event, int(enemy_spawn_cooldown))
 
     #input
     keys = pygame.key.get_pressed()
     all_sprites.update(keys, deltatime)
-    
     laser_collisions()
     player_collisions()
 
+    print(f'movement speed: {player.speed}, cooldown: {player.cooldown_time}')
+
     #draw the game
     if running:
-        screen.fill('#2c205a')
-        all_sprites.draw(screen)
-        display_time_passsed()
+        draw_window()
         pygame.display.update()
+
 pygame.quit()
